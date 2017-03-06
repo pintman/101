2c0d4a11deea7ca715e2940f56c01a95bc911c18
Marco Bakera <bakera@tbs1.de>
2017-03-06 16:32:25 +0100

    information about default pins.

--- prog/eapi.py
+++ prog/eapi.py
@@ -13,3 +13,11 @@ def taster0_gedrueckt(pin):
   print("Taster 0 wurde gedrückt.")
 eam = EAModul()
 eam.taster_event_registrieren(0, taster0_gedrueckt)
+
+
+# EA-Modul
+# Standard Pin-Belegung:
+# 3,3V=1 
+# Taster0=29, Taster1=31,
+# LED: rot=33, gelb=35, gruen=37
+# GND=39
